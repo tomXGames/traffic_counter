18c4f5ca500921850adbb78a7fa138f873da91a0
tomXGames <tommaso.peduzzi@gmx.ch>
2021-01-06 11:49:14 +0100

    First Commit
    Should be first working version. Still looks like crap though.

---
+++ lib/main.dart
@@ -0,0 +1,107 @@
+import 'package:flutter/material.dart';
+import 'package:shared_preferences/shared_preferences.dart';
+void main() {
+  runApp(TabBarDemo());
+}
+
+class TabBarDemo extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: DefaultTabController(
+        length: 3,
+        child: Scaffold(
+          appBar: AppBar(
+            bottom: TabBar(
+              tabs: [
+                Tab(icon: Icon(Icons.directions_car)),
+                Tab(icon: Icon(Icons.motorcycle)),
+                Tab(icon: Icon(Icons.directions_bike)),
+              ],
+            ),
+            title: Text('Tabs Demo'),
+          ),
+          body: TabBarView(
+            children: [
+              TrafficCounter(title: 'Cars', icon: Icons.directions_car),
+              TrafficCounter(title: 'Motorcycle', icon: Icons.motorcycle),
+              TrafficCounter(title: 'Bikes', icon: Icons.directions_bike),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
+class TrafficCounter extends StatefulWidget {
+  TrafficCounter({Key key, this.title, this.icon}) : super(key: key);
+  String title;
+  IconData icon;
+  @override
+  _TrafficCounter createState() => new _TrafficCounter();
+}
+class _TrafficCounter extends State<TrafficCounter> {
+  int _counter = 0;
+
+  void _incrementCounter(String type)  {
+    setState(() async {
+      final prefs = await SharedPreferences.getInstance();
+      int curval = prefs.getInt(type);
+      prefs.setInt(type, curval+1);
+    });
+  }
+
+  void _getCounter(String type)  {
+    setState(() async {
+      final prefs = await SharedPreferences.getInstance();
+      _counter = prefs.getInt(type);
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    // This method is rerun every time setState is called, for instance as done
+    // by the _incrementCounter method above.
+    //
+    // The Flutter framework has been optimized to make rerunning build methods
+    // fast, so that you can just rebuild anything that needs updating rather
+    // than having to individually change instances of widgets.
+    return Scaffold(
+      body: Center(
+        // Center is a layout widget. It takes a single child and positions it
+        // in the middle of the parent.
+        child: Column(
+          // Column is also layout widget. It takes a list of children and
+          // arranges them vertically. By default, it sizes itself to fit its
+          // children horizontally, and tries to be as tall as its parent.
+          //
+          // Invoke "debug painting" (press "p" in the console, choose the
+          // "Toggle Debug Paint" action from the Flutter Inspector in Android
+          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
+          // to see the wireframe for each widget.
+          //
+          // Column has various properties to control how it sizes itself and
+          // how it positions its children. Here we use mainAxisAlignment to
+          // center the children vertically; the main axis here is the vertical
+          // axis because Columns are vertical (the cross axis would be
+          // horizontal).
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: <Widget>[
+            Icon(
+              widget.icon,
+            ),
+            Text(
+              '$_counter',
+              style: Theme.of(context).textTheme.headline4,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: () => _incrementCounter(widget.title),
+        tooltip: 'Increment',
+        child: Icon(Icons.add),
+      ), // This trailing comma makes auto-formatting nicer for build methods.
+    );
+  }
+}
